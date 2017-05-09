9d649382dc84fb70c23f71e8fbe45c20bacc6933
xCoBaLTz <seananchabra@hotmail.com>
2017-05-08 23:50:35 -0400

    making sure user is logged in

--- project.py
+++ project.py
@@ -123,6 +123,10 @@ def gconnect():
     print "done!"
     return output
 
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
 
 @app.route('/gdisconnect')
 def gdisconnect():
