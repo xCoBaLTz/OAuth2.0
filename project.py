f771d224b57c05adc1aa270911417b9f757304d3
xCoBaLTz <seananchabra@hotmail.com>
2017-05-20 17:07:20 -0400

    working facebook login

--- project.py
+++ project.py
@@ -49,29 +49,27 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
-    app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print result
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    #userinfo_url = "https://graph.facebook.com/v2.9/me"
     # strip expire tag from access token
-    token = json.loads(result)
+    data = json.loads(result)
+    token = 'access_token=' + data['access_token']
 
-    url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token['access_token']
+    url = 'https://graph.facebook.com/v2.9/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print result
+    
     # print "url sent for API access:%s"% url
     # print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
-    login_session['username'] = data["username"]
+    login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
@@ -80,7 +78,7 @@ def fbconnect():
     login_session['access_token'] = stored_token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.9/me/picture?%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
